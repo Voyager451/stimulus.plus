3784188b92a40ba458836fd9eb19374c2a634403
Voyager451 <55966872+Voyager451@users.noreply.github.com>
2020-03-30 18:51:33 -0400

    Update head tags

--- pages/_document.js
+++ pages/_document.js
@@ -8,12 +8,17 @@ class MyDocument extends Document {
         return { ...initialProps };
     }
 
-    static render() {
+    render() {
+
+        const siteUrl = 'https://stimulus.plus';
+        const siteTitle = 'Stimulus Payment Calculator';
+        const siteDesc = 'Calculate what you will get from COVID-19 (Coronavirus) stimulus payments/checks.';
+
         return (
             <html>
                 <Head>
                     <meta charSet="utf-8"/>
-                    <meta name="description" content="Calculate what you will get from COVID-19 (Coronavirus) stimulus payments/checks."/>
+                    <meta name="description" content={ siteDesc } />
                     <meta name="keywords"
                         content="stimulus calculator, stimulus payment calculator, stimulus check calculator, coronavirus stimulus calculator, coronavirus payment calculator, covid-19 payment calculator"/>
                     <meta name="viewport"
@@ -22,6 +27,11 @@ class MyDocument extends Document {
                         href="https://fonts.googleapis.com/css?family=Montserrat:500,700"
                         rel="stylesheet"
                     />
+
+                    <link rel="canonical" href={ siteUrl } />
+                    <meta property="og:type" content="website" />
+                    <meta name="og:title" property="og:title" content={ siteTitle } />
+                    <meta name="og:description" property="og:description" content={ siteDesc } />
                 </Head>
                 <body>
                     <Main />
